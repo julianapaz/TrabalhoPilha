f66193fe04b96e5422b74d3f4bdd2271251e1e57
Juliana Paz <julianapaz07@hotmail.com>
2013-10-27 22:47:17 -0200

    Update Planilha.java

--- src/com/senac/Planilha.java
+++ src/com/senac/Planilha.java
@@ -6,21 +6,25 @@ import com.senac.algoritmos.AvaliadorRPN;
 
 import static java.lang.System.*;
 
+
+
+
+
 public class Planilha {
+
 	public static void main(String[] args) throws Exception {
 		out.println( AvaliadorRPN.avalia("2 3 4 + *") );
-	
-	Scanner entradaTeclado = new Scanner(System.in);
-	String expressao="";
-	while(entradaTeclado.hasNext()){
-		expressao=entradaTeclado.nextLine();
-		if(expressao.equals("fim"))
-			System.exit(0);
-		else
-			AvaliadorRPN.inversorPosFixo(expressao);
-		
-	}
-	
-	
+
+		Scanner entradaTeclado = new Scanner(System.in);
+		String expressao="( 20 * ( 5 + 4 ) )";
+		while ( entradaTeclado.hasNext() )
+		{
+			expressao=entradaTeclado.nextLine();
+			if(expressao.equals("fim"))
+				System.exit(0);
+			else
+				out.print(AvaliadorRPN.inversorPosFixo(expressao));
+
+		}
 	}
 }
