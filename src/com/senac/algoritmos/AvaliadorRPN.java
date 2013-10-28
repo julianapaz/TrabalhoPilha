e5574b255938c8f8208436d6b3ca3394fcf74b17
Juliana Paz <julianapaz07@hotmail.com>
2013-10-27 22:48:08 -0200

    Update AvaliadorRPN.java

--- src/com/senac/algoritmos/AvaliadorRPN.java
+++ src/com/senac/algoritmos/AvaliadorRPN.java
@@ -1,7 +1,7 @@
 package com.senac.algoritmos;
 
 import java.util.Scanner;
-
+import static java.lang.System.*;
 import com.senac.estruturas.PilhaCheia;
 import com.senac.estruturas.PilhaOperador;
 import com.senac.estruturas.PilhaOperando;
@@ -28,14 +28,11 @@ public class AvaliadorRPN {
 		return pilha.pop();
 	}
 
-
-
-	public static void inversorPosFixo(String expressao)throws PilhaCheia, PilhaVazia, InvalidOperator{
+	public static double inversorPosFixo(String expressao)throws PilhaCheia, PilhaVazia, InvalidOperator{
 		Scanner entrada= new Scanner(expressao);
 
 		PilhaOperador pilha = new PilhaOperador(50);
 		String saida = "";
-
 
 		while (entrada.hasNext())
 		{
@@ -49,133 +46,98 @@ public class AvaliadorRPN {
 			{
 				String opLido = entrada.next();
 
-				/*Se for o operador ")", desempilhar ate que o
-			   		operador "(" seja o operador "desempilhado";
-			   		a cada operador desempilhado, enviar para a
-			   		sa’da (com excecao do "(").*/
-				if	(ehFechaParentes(opLido)){
+				if	( ehFechaParentes(opLido) ){
 					do{
-						//  if(ehAbreParenteses(pilha.peek()))
-						//  pilha.pop();
-						//else
+
 						saida += " " + pilha.pop();
 
-					}while(!ehAbreParenteses(pilha.peek()));	  
+					}while( !ehAbreParenteses(pilha.peek()) );
+					
+					pilha.pop();
 				}
-
-				/*Sen‹o, se o operador tiver prioridade MAIOR
-			   		que o operador do topo da pilha, ou se a pilha 
-			   		estiver vazia, ou o operador do topo da pilha
-			   		for "(", empilhar o operador.*/
 
 				else 
 				{				
 					if	( pilha.isEmpty() || ehAbreParenteses(pilha.peek()) )
-					{
+					{		
+						pilha.push(opLido);
+					}
 
-					//if(prioridade(opLido)>prioridade(pilha.peek()))
+					else if ( prioridade(opLido)>prioridade(pilha.peek()) )
 						pilha.push(opLido);
 
-					/*Sen‹o, dempilhar (enviando para a sa’da) os
-				   operadores da pilha atŽ que o operador tenha
-				   prioridade MAIOR que o operador do topo, ou
-				   que o operador do topo da pilha seja "(",
-				   ou que a pilha esteja vazia. Empilhar o operador.*/
-					}
-					else if (prioridade(opLido)>prioridade(pilha.peek()))
-							pilha.push(opLido);
-
-					else
+					else				
 					{
-						while	(prioridade(opLido)<prioridade(pilha.peek()) || !ehAbreParenteses(pilha.peek()) || !pilha.isEmpty())
+						while	( !pilha.isEmpty() && prioridade(opLido)<prioridade(pilha.peek()) && !ehAbreParenteses(pilha.peek()) )
 						{
-							if	(ehAbreParenteses(pilha.peek()))
-								pilha.pop();
-							else
-								saida +=" "+pilha.pop();
+							saida +=" "+pilha.pop();
 						}
-					//Empilhar o operador.
+
 						pilha.push(opLido);
 					}
+				}
 			}
 		}
-			}
-
-
-
-	/* Desempilhar os operadores da pilha enviando para a sa’da atŽ
-   que a pilha fique vazia. Nenhum desse operadores pode ser o "(".*/
-
-
-	while	(!pilha.isEmpty())
-	{
-		if	(ehAbreParenteses(pilha.peek()))
-			pilha.pop();
-		else			
+		while	( !pilha.isEmpty() )
+		{
 			saida += " "+pilha.pop();
-	}	
+		}	
 
-	System.out.print("Notação"+saida+"\n");	
-	System.out.print(AvaliadorRPN.avalia(saida));
-}
-
-public static boolean ehAbreParenteses(String op) {
-	return op.equals("(");
-}
-
-public static boolean ehFechaParentes(String op) {
-	return op.equals(")");
-
-}
-
-public static int prioridade(String operador) throws InvalidOperator {
-	int precedencia = 0;
-	switch (operador.charAt(0)) {
-	case '(':
-		precedencia = 3;
-		break;
-
-	case '*':
-		precedencia = 2;
-		break;
-
-	case '/':
-		precedencia = 2;
-		break;
-
-	case '+':
-		precedencia = 1;
-		break;
-
-	case '-':
-		precedencia = 1;
-		break;
-
-	//n precisa
-	case ')':
-		precedencia = 0;
-		break;
-
-	default:
-		throw new InvalidOperator(operador.charAt(0));
+		return avalia(saida);
 	}
 
-	return precedencia;
-}
-
-private static double executaOperador(char op, double lhs, double rhs)
-		throws InvalidOperator {
-	switch (op) {
-	case '+':
-		return lhs + rhs;
-	case '-':
-		return lhs - rhs;
-	case '*':
-		return lhs * rhs;
-	case '/':
-		return lhs / rhs;
-	default:
-		throw new InvalidOperator(op);
+	public static boolean ehAbreParenteses(String op) {
+		return op.equals("(");
 	}
-}
+
+	public static boolean ehFechaParentes(String op) {
+		return op.equals(")");
+
+	}
+
+	public static int prioridade(String operador) throws InvalidOperator {
+		int precedencia = 0;
+		switch (operador.charAt(0)) {
+		case '(':
+			precedencia = 3;
+			break;
+
+		case '*':
+			precedencia = 2;
+			break;
+
+		case '/':
+			precedencia = 2;
+			break;
+
+		case '+':
+			precedencia = 1;
+			break;
+
+		case '-':
+			precedencia = 1;
+			break;
+
+		default:
+			throw new InvalidOperator(operador.charAt(0));
+		}
+
+		return precedencia;
+	}
+
+	private static double executaOperador(char op, double lhs, double rhs)
+			throws InvalidOperator {
+		switch (op) {
+		case '+':
+			return lhs + rhs;
+		case '-':
+			return lhs - rhs;
+		case '*':
+			return lhs * rhs;
+		case '/':
+			return lhs / rhs;
+		default:
+			throw new InvalidOperator(op);
+		}
+	}
 }
